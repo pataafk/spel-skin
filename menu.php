0220b481442df2384463d16bce50ad0b323a7c78
edvin johansson <redeye0700@hotmail.se>
2018-11-13 14:35:32 +0100

    yes

---
+++ menu.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * mall för menyn
+ */
+?>
+<nav> 
+    <ul class="menu"> 
+      <li><a href="om‐oss.html">Vilka vi är &#x3020;</a></li> 
+      <li><a href="arbeten.html">Vad vi gör &#x2318;</a></li> 
+      <li><a href="blog.php">Blogg &#x272A;</a></li> 
+      <li><a href="account.php">Kontakt &#x260E;</a></li> 
+    </ul> 
+  </nav>
