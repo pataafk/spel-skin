5f4e2ec9beddda9aaf28548c3af60da50debdad0
edvin johansson <redeye0700@hotmail.se>
2018-12-12 13:59:11 +0100

    index.php

---
+++ html/Index.php
@@ -0,0 +1,32 @@
+<?
+
+
+?>
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+  <meta charset="utf-8" />
+  <title>Dö</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link href="css/spel.css" rel="stylesheet" />
+  
+</head>
+<body class="startpage">
+  <header>
+    <p class="tagline"></p>
+  </header>
+  <div id="mySidenav" class="menu">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="#">UNSA</a>
+  <a href="#">Dawid</a>
+  <a href="#">take controll</a>
+</div>
+  
+
+  <script src="script/sidenav.js"></script>
+  <span style="font-size:30px;cursor:pointer" onclick="openNav()">welcome</span>
+  <footer>
+    <small>&copy;</small>
+  </footer>
+</body>
+</html>
